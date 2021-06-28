5d0a623fcaa5a9c7331f34b3c6a8433922f956c5
juliannah5203 <jiejinghe@gmail.com>
2021-06-28 03:13:20 -0700

    add the explode animation

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,20 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+      super(scene, x, y, texture, frame);
+  
+      // add object to existing scene
+      scene.add.existing(this);
+      this.points = pointValue; // store pointValue
+      this.moveSpeed = 3; //pixels per frame
+
+    }
+    update() {
+        this.x -= this.moveSpeed;
+        if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+    reset() {
+        this.x = game.config.width;
+    }
+}
