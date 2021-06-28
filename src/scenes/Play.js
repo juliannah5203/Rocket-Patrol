68d8ce0fc381f25dcb8fdc580643ba6d91cff472
juliannah5203 <jiejinghe@gmail.com>
2021-06-28 04:37:43 -0700

    corrected the Restart function

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -62,6 +62,7 @@ class Play extends Phaser.Scene {
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
         }, null, this);
 
     }
